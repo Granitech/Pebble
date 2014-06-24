6c3f38c4a8e6534d0303b70364f686604d037329
Russell Fair <rfair404@gmail.com>
2014-06-24 18:18:38 -0400

    reduce header brand size a bit

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '98afbc66227128ebbaf3015552c37efe');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a847a3c6db25883289995c233d4f81c4');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
