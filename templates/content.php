f479a3abd468f7cef0d174c9fbbf849ba9c5321b
Russell Fair <rfair404@gmail.com>
2014-06-24 14:45:28 -0400

    add the header and secondary nav

--- templates/content.php
+++ templates/content.php
@@ -1,8 +1,15 @@
 <article <?php post_class(); ?>>
   <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
+     <?php if(has_post_thumbnail()){ ?>
+   <figure class="featured-image">
+   <a href="<?php echo get_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+         <?php the_post_thumbnail(); ?>
+  </a>
+  </figure>
+  <?php } ?>
   <div class="entry-summary">
     <?php the_excerpt(); ?>
   </div>
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -19,3 +19,19 @@
     </nav>
   </div>
 </header>
+<section class="banner masthead" role="banner">
+    <figure>
+      <img src="<?php header_image() ?>" alt="" class="header-image" />
+    <!--   <a class="brand" href="<?php echo home_url('/') ?>"><?php bloginfo('name'); ?></a> -->
+  </figure>
+</section>
+<section class="banner navbar navbar-default" role="banner">
+  <div class="container">
+        <nav class="collapse navbar-collapse" role="navigation">
+        <?php if (has_nav_menu('secondary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'secondary_navigation', 'menu_class' => 'nav navbar-nav'));
+        endif;
+        ?>
+      </nav>
+  </div>
+</section>
