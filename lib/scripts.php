4419b9738b21f8178742af407176708fa48f5cbd
Russell Fair <rfair404@gmail.com>
2014-06-24 18:07:39 -0400

    some more css tweaks

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fd770f6ecddc50b324119bb0b3960676');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '98afbc66227128ebbaf3015552c37efe');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
