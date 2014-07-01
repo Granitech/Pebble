fe6ba595fcf705e58556d45a63644da5b148e629
Russell Fair <rfair404@gmail.com>
2014-07-01 08:27:12 -0400

    smaller logo, smaller header image

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -2,14 +2,13 @@
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
+        <span class="sr-only"><?php __('Toggle navigation', 'pebble' ); ?></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
-
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
@@ -19,19 +18,11 @@
     </nav>
   </div>
 </header>
+<?php if ( is_home() ) { ?>
 <section class="banner masthead" role="banner">
     <figure>
       <img src="<?php header_image() ?>" alt="" class="header-image" />
     <!--   <a class="brand" href="<?php echo home_url('/') ?>"><?php bloginfo('name'); ?></a> -->
   </figure>
 </section>
-<section class="banner navbar navbar-secondary" role="banner">
-  <div class="container">
-        <nav class="collapse navbar-collapse" role="navigation">
-        <?php if (has_nav_menu('secondary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'secondary_navigation', 'menu_class' => 'nav navbar-nav'));
-        endif;
-        ?>
-      </nav>
-  </div>
-</section>
+<?php }
