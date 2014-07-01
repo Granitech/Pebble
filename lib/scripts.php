9d153f26abef4948bc9b24be38ab6a9bfea2935f
Russell Fair <rfair404@gmail.com>
2014-07-01 09:22:33 -0400

    larger summaries

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '334caf1bd42c501d15670544712ae20c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '91ad05e60dc6712b95c4dab59601eed5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
