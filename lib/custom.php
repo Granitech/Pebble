a0d3aaf4137b8dd1f2860d6f61e6c4bb64dd9846
Russell Fair <rfair404@gmail.com>
2014-07-01 09:52:06 -0400

    revert to full width images

--- lib/custom.php
+++ lib/custom.php
@@ -14,3 +14,20 @@ $header_args = array(
   'default-image' => get_template_directory_uri() . '/assets/img/default-header.jpg',
 );
 add_theme_support( 'custom-header', $header_args );
+
+
+// <link href="http://fonts.googleapis.com/css?family=Istok+Web" rel="stylesheet" type="text/css">
+// <link href="http://fonts.googleapis.com/css?family=Lora" rel="stylesheet" type="text/css">
+
+function pebble_register_font_stack(){
+    $pebble_version = '023';
+    wp_register_style('istok-web', 'http://fonts.googleapis.com/css?family=Istok+Web', array(), $pebble_version, 'all');
+    wp_register_style('lora', 'http://fonts.googleapis.com/css?family=Lora', array(), $pebble_version, 'all');
+}
+add_action( 'init', 'pebble_register_font_stack' );
+
+function pebble_enqueue_font_stack(){
+    wp_enqueue_style('istok-web');
+    wp_enqueue_style('lora');
+}
+add_action( 'wp_enqueue_scripts', 'pebble_enqueue_font_stack' );
--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '91ad05e60dc6712b95c4dab59601eed5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6179fbb224649061e2cb8b495f84c71d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
